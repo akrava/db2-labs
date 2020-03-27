4d427653843cf5e007c7f2f762a293718b612de0
Arkadiy <arkadiy@ex.ua>
2020-03-27 16:09:36 +0200

    fix scripts

--- lab2/bootstrap.sh
+++ lab2/bootstrap.sh
@@ -7,18 +7,16 @@ if ! pgrep -x "redis-server" >/dev/null; then
     exit 1
 fi
 
-if [[ ! -d venv ]]; then
-    virtualenv venv --python=python3.6
-    source venv/bin/activate
-    pipenv install --dev
-else
-    source venv/bin/activate
+if [[ ! -f .env ]]; then
+    cp  .env.example .env
 fi
 
+pipenv install --dev
+
 if [[ $# -eq 1 ]] && ([[ $1 = "--cui" ]] || [[ $1 = "--worker" ]]); then
-    python src/main.py $1
+     pipenv run python src/main.py $1
 elif [[ $# -eq 3 ]] && [[ $1 = "--simulate" ]]; then
-    python src/simulation.py $2 $3
+     pipenv run python src/simulation.py $2 $3
 else
     echo "No arguments supplied. Need --cui or --worker"
     exit 1
